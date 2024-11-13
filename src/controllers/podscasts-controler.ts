8b3f255eb4ff29a41970c4b7c1c197a420b69241
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-11-13 16:17:00 -0300

    Chamada para api com simuladores de clients

--- src/controllers/podscasts-controler.ts
+++ src/controllers/podscasts-controler.ts
@@ -5,5 +5,20 @@ export const getListEpisodes = async (
   res: ServerResponse
 ) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Get list of episodes' }));
+  res.end(
+    JSON.stringify([
+      {
+        podcastName: 'flow',
+        episode: 'CBUM - Flow #319',
+        videoId: 'pQSuQmUfS30',
+        categories: ['saúde', 'esporte', 'bodybuilder'],
+      },
+      {
+        podcastName: 'flow',
+        episode: 'RUBENS BARRICHELLO - Flow #339',
+        videoId: '4KDGTdiOV4I',
+        categories: ['esporte', 'corrida'],
+      },
+    ])
+  );
 };
