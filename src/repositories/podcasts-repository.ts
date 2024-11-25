4ffc2a8dfa074e6fde22fb2c7c70c9b98708d859
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-11-25 08:38:04 -0300

    Criando o Service

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -3,13 +3,24 @@ import path from 'path';
 import { PodcastModel } from '../models/podcast-model';
 
 // Ajuste o caminho para o JSON
-const pathData = path.resolve(__dirname, '../../src/repositories/podcasts.json');
+const pathData = path.resolve(
+  __dirname,
+  '../../src/repositories/podcasts.json'
+);
 
-export const repositoryPodcast = async (): Promise<PodcastModel[]> => {
+export const repositoryPodcast = async (
+  podcastName?: string
+): Promise<PodcastModel[]> => {
   try {
     const rawData = await fs.promises.readFile(pathData, 'utf-8');
-    const jsonFile = JSON.parse(rawData);
-    
+    let jsonFile = JSON.parse(rawData);
+
+    if (podcastName) {
+      jsonFile = jsonFile.filter(
+        (podcast: PodcastModel) => podcast.podcastName === podcastName
+      );
+    }
+
     return jsonFile;
   } catch (error) {
     throw error;
